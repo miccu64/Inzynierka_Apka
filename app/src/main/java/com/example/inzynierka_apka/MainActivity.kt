7466e5e2503633d2ef98e116e45d596a3f20d972
Konrad M <98konrad98@gmail.com>
2020-08-10 14:20:45 +0200

    Poprawki w wysyłaniu

--- app/src/main/java/com/example/inzynierka_apka/MainActivity.kt
+++ app/src/main/java/com/example/inzynierka_apka/MainActivity.kt
@@ -1,27 +1,25 @@
 package com.example.inzynierka_apka
 
 import Localization
-import android.Manifest
-import android.content.pm.PackageManager
 import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.app.ActivityCompat
 import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonArrayRequest
+import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.rest.QueueSingleton
 import com.example.rest.ServerOperations
-import org.json.JSONArray
 import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
+    private val server: String = "https://192.168.2.4:45455"
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -31,12 +29,31 @@ class MainActivity : AppCompatActivity() {
         //ignores every certificate of SSL!!!!!!!!!!!!!!
         NukeSSLCerts.nuke()
 
-        //myGet()
-        //getUsers2()
-        print("aaaaa")
+        myGet()
     }
 
+    private fun <T> addToQueue(request: Request<T>) {
+        QueueSingleton.getInstance(this).addToRequestQueue(request)
+    }
 
+    private fun postData(lat: Double, lon: Double) {
+        val url = "$server/"
+        val jsonObject = JSONObject()
+        jsonObject.put("id", "mmmaaa")
+        jsonObject.put("latitude", lat)
+        jsonObject.put("longitude", lon)
+
+
+        val request = JsonObjectRequest(Request.Method.POST, url, jsonObject,
+            Response.Listener { response ->
+                Log.d("TAG", response.toString())
+            },
+            Response.ErrorListener { error ->
+                Log.d("TAG", error.toString())
+            }
+        )
+        addToQueue(request)
+    }
 
     fun showLocalization(view: View) {
         val local = Localization(this, this)
@@ -46,76 +63,25 @@ class MainActivity : AppCompatActivity() {
         print(latitude)
         print("aaaaa")
         Log.d("TAG", latitude.toString())
+        postData(latitude, longitude)
     }
 
-    fun myGet() {
+    private fun myGet() {
 
 
-        val url: String = "https://192.168.2.9:45455/playerInfo"
+        val url: String = "$server/playerInfo"
 
         val jsonObjectRequest = JsonArrayRequest(Request.Method.GET, url, null,
-                Response.Listener { response ->
-                    textView!!.text = "response :  "
-                    //textView.text = "Response: %s".format(response.toString())
-                },
-                Response.ErrorListener { error ->
-                    textView!!.text = error.toString()
-
-                }
+            Response.Listener { response ->
+                textView!!.text = "response :  "
+                //textView.text = "Response: %s".format(response.toString())
+            },
+            Response.ErrorListener { error ->
+                textView!!.text = error.toString()
+            }
         )
 
-
-        // Get a RequestQueue
-        val queue = QueueSingleton.getInstance(this.applicationContext).requestQueue
-        // Add a request (in this example, called stringRequest) to your RequestQueue.
-        QueueSingleton.getInstance(this).addToRequestQueue(jsonObjectRequest)
-
-        /*
-        // Request a string response from the provided URL.
-        val stringReq = StringRequest(Request.Method.GET, url,
-            Response.Listener<String> { response ->
-
-                var strResp = response.toString()
-                val jsonObj: JSONObject = JSONObject(strResp)
-                val jsonArray: JSONArray = jsonObj.getJSONArray("items")
-                var str_user: String = ""
-
-                textView!!.text = "response :  "
-            },
-            Response.ErrorListener { textView!!.text = "That didn't work!" })
-        queue.add(stringReq)*/
+        addToQueue(jsonObjectRequest)
     }
 
-
-    fun getUsers2()
-    {
-        var server = ServerOperations()
-        var queue = Volley.newRequestQueue(this)
-        var (my,queue2) = server.myGet(queue)
-        textView!!.text = my
-    }
-
-    // function for network call
-    fun getUsers() {
-        // Instantiate the RequestQueue.
-        val queue = Volley.newRequestQueue(this)
-        val url: String = "https://api.github.com/search/users?q=eyehunt"
-
-        // Request a string response from the provided URL.
-        val stringReq = StringRequest(Request.Method.GET, url,
-                Response.Listener<String> { response ->
-
-                    var strResp = response.toString()
-                    val jsonObj: JSONObject = JSONObject(strResp)
-                    val jsonArray: JSONArray = jsonObj.getJSONArray("items")
-                    var str_user: String = ""
-                    for (i in 0 until jsonArray.length()) {
-                        var jsonInner: JSONObject = jsonArray.getJSONObject(i)
-                        str_user = str_user + "\n" + jsonInner.get("login")
-                    }
-                    textView!!.text = "response : $str_user "
-                },
-                Response.ErrorListener { textView!!.text = "That didn't work!" })
-        queue.add(stringReq)
-    }
 }
