9973d5b4a043794a7743fdb6feefbdfb7de90728
Konrad M <98konrad98@gmail.com>
2020-10-12 20:30:23 +0200

    SignalR disconnected

--- app/src/main/java/com/example/inzynierka_apka/MainActivity.kt
+++ app/src/main/java/com/example/inzynierka_apka/MainActivity.kt
@@ -14,22 +14,27 @@ import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.example.rest.QueueSingleton
 import com.example.rest.ServerOperations
+import com.microsoft.signalr.HubConnection
+import com.microsoft.signalr.HubConnectionBuilder
 import org.json.JSONObject
 
 
 class MainActivity : AppCompatActivity() {
     private var textView: TextView? = null
-    private val server: String = "https://192.168.2.4:45455"
+    private val server: String = "https://192.168.2.2:45456"
+    private lateinit var hubConnection: HubConnection
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         textView = findViewById<TextView>(R.id.textView)
-
+        hubConnection = HubConnectionBuilder.create("$server/gamehub").build()
         //ignores every certificate of SSL!!!!!!!!!!!!!!
         NukeSSLCerts.nuke()
-
         myGet()
+
+        hubConnection.start()
+
     }
 
     private fun <T> addToQueue(request: Request<T>) {
@@ -37,9 +42,9 @@ class MainActivity : AppCompatActivity() {
     }
 
     private fun postData(lat: Double, lon: Double) {
-        val url = "$server/playerinfo/send"
+        val url = "$server/coords/register"
         val jsonObject = JSONObject()
-        jsonObject.put("Id", "ddhdddddfd")
+        jsonObject.put("Id", "DAAAAXD")
         jsonObject.put("Longitude", lon)
         jsonObject.put("Latitude", lat)
 
@@ -64,12 +69,15 @@ class MainActivity : AppCompatActivity() {
         print("aaaaa")
         Log.d("TAG", latitude.toString())
         postData(latitude, longitude)
+        TODO("Ciagle jest Disconnected :C")
+        val resp = hubConnection.connectionState//hubConnection.send("CreateRoom", "input")
+        print(resp)
     }
 
     private fun myGet() {
 
 
-        val url: String = "$server/playerInfo"
+        val url: String = "$server/coords/getById/2"
 
         val jsonObjectRequest = JsonArrayRequest(Request.Method.GET, url, null,
             Response.Listener { response ->
